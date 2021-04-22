a02a2031852f1773491d27c1ce6c926107a5c193
syed <faesal.zaydi@hotmail.com>
2021-04-22 16:38:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mustache = require('mustache-express');
+const bodyParser = require("body-parser");
+
+
+
+
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+
+const public = path.join(__dirname, 'public');
+app.use(express.static(public));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const router = require('./routes/fitnessAppRoutes');
+app.use('/', router);
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000. Ctrl^c to quit.');
+})
